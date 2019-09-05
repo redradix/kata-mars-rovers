27388e557de114c4a8461615cb7afea2b057ff84
ALVARO <alvaro.vazquez@redradix.com>
2019-09-05 12:21:35 +0200

    Added the movement commands and some tests.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,60 @@
 const rotate = (position, direction, command) => {
-  const rotateLeft = {
-    N: 'W',
-    W: 'S',
-    S: 'E',
-    E: 'N'
-  };
-  const rotateRight = {
-    N: 'E',
-    E: 'S',
-    S: 'W',
-    W: 'N'
+  const rotation = {
+    l: {
+      N: 'W',
+      W: 'S',
+      S: 'E',
+      E: 'N'
+    },
+    r: {
+      N: 'E',
+      E: 'S',
+      S: 'W',
+      W: 'N'
+    }
   };
 
   return {
-    position: position,
-    direction: command === 'l' ? rotateLeft[direction] : rotateRight[direction]
+    position,
+    direction: rotation[command][direction]
+  };
+};
+
+const isOutsideGrid = (newPosition, gridSize) => {
+  return (
+    newPosition[0] >= gridSize[0] ||
+    newPosition[1] >= gridSize[1] ||
+    newPosition[0] < 0 ||
+    newPosition[1] < 0
+  );
+};
+
+const move = (position, direction, gridSize, command) => {
+  const movement = {
+    f: {
+      N: [0, 1],
+      S: [0, -1],
+      E: [1, 0],
+      W: [-1, 0]
+    },
+    b: {
+      N: [0, -1],
+      S: [0, 1],
+      E: [-1, 0],
+      W: [1, 0]
+    }
+  };
+
+  const movementToApply = movement[command][direction];
+  const newPosition = [
+    position[0] + movementToApply[0],
+    position[1] + movementToApply[1]
+  ];
+
+  // only returns the new position if it's not outside of grid
+  return {
+    position: !isOutsideGrid(newPosition, gridSize) ? newPosition : position,
+    direction
   };
 };
 
@@ -23,6 +63,9 @@ const processCommand = (position, direction, gridSize, command) => {
     case 'l':
     case 'r':
       return rotate(position, direction, command);
+    case 'f':
+    case 'b':
+      return move(position, direction, gridSize, command);
     default:
       return { position, direction };
   }
--- src/index.test.js
+++ src/index.test.js
@@ -3,10 +3,6 @@ const { startExploration } = require('./index');
 describe('Mars Rovers', () => {
   const gridSize = [3, 3];
 
-  test('test to delete', () => {
-    expect(4).toBe(4);
-  });
-
   test('stays in the same position for an invalidad command', () => {
     expect(startExploration([1, 1], 'N', gridSize, 'x')).toEqual({
       position: [1, 1],
@@ -25,6 +21,27 @@ describe('Mars Rovers', () => {
     expect(startExploration([1, 1], 'N', gridSize, 'r')).toEqual({
       position: [1, 1],
       direction: 'E'
+    });
+  });
+
+  test('stays in the same position for trying to move the rover out of the grid', () => {
+    expect(startExploration([2, 2], 'N', gridSize, 'f')).toEqual({
+      position: [2, 2],
+      direction: 'N'
+    });
+  });
+
+  test('move the rover forward', () => {
+    expect(startExploration([1, 1], 'N', gridSize, 'f')).toEqual({
+      position: [1, 2],
+      direction: 'N'
+    });
+  });
+
+  test('move the rover backward', () => {
+    expect(startExploration([1, 1], 'N', gridSize, 'b')).toEqual({
+      position: [1, 0],
+      direction: 'N'
     });
   });
 });
