c74e8b04419e70ac3f378df4e3bec22707fa4dee
Sonia Ruiz <soniaruiz@MacBook-Pro-de-Sonia.local>
2020-04-08 16:57:56 +0200

    Refactor

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ const move = (initialPosition) => {
   const [x, y, orientation] = initialPosition
 
   if (orientation === 'E') {
-    return [x+1, y, 'E']
+    return [x+1, y, orientation]
   }
   
   if (orientation === 'S') {
-    return [1, 0, 'S']
+    return [1, 0, orientation]
   }
 
   if (orientation === 'W') {
-    return [1, 2, 'W']
+    return [1, 2, orientation]
   }
 
-  return [x, y+1, 'N']
+  return [x, y+1, orientation]
 }
 
 module.exports = move
