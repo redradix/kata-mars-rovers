5cbbcb326a7fd9a7dccc5892424e2094e3296418
Sonia Ruiz <soniaruiz@MacBook-Pro-de-Sonia.local>
2020-04-08 16:56:45 +0200

    Red

--- src/index.test.js
+++ src/index.test.js
@@ -9,8 +9,12 @@ describe('Mars Rovers', () => {
     expect(move([2, 2, 'N'], 7, 'f')).toEqual([2, 3, 'N'])
   })
 
-  it('When facing east and moving fordward, increase the x coord', () => {
+  it('When facing east and moving fordward, increase the x coord (1)', () => {
     expect(move([1, 1, 'E'], 7, 'f')).toEqual([2, 1, 'E'])
+  })
+
+  it('When facing east and moving fordward, increase the x coord (2)', () => {
+    expect(move([2, 2, 'E'], 7, 'f')).toEqual([3, 2, 'E'])
   })
 
   it('When facing south and moving fordward, decrease the y coord', () => {
