4f7ebf35b1ff1b71f80dd1f4ee3ba98047380965
Sonia Ruiz <soniaruiz@MacBook-Pro-de-Sonia.local>
2020-04-08 16:55:17 +0200

    Green

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const move = (initialPosition) => {
   const orientation = initialPosition[2]
+  const y = initialPosition[1]
+  const x = initialPosition[0]
 
   if (orientation === 'E') {
     return [2, 1, 'E']
@@ -13,7 +15,7 @@ const move = (initialPosition) => {
     return [1, 2, 'W']
   }
 
-  return [1, 2, 'N']
+  return [x, y+1, 'N']
 }
 
 module.exports = move
