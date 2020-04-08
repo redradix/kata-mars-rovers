ebb14a4058bd1d3b96d9b27b191c95b59b436454
Sonia Ruiz <soniaruiz@MacBook-Pro-de-Sonia.local>
2020-04-08 17:05:47 +0200

    Refactor

--- src/index.test.js
+++ src/index.test.js
@@ -1,22 +1,22 @@
 const move = require('./index')
 
 describe('Mars Rovers', () => {
-  it('When facing north and moving fordward, increase the y coord (1)', () => {
+  it('When facing north and moving fordward, increase the y coord', () => {
     expect(move([1, 1, 'N'], 'f')).toEqual([1, 2, 'N'])
     expect(move([2, 2, 'N'], 'f')).toEqual([2, 3, 'N'])
   })
 
-  it('When facing east and moving fordward, increase the x coord (1)', () => {
+  it('When facing east and moving fordward, increase the x coord', () => {
     expect(move([1, 1, 'E'], 'f')).toEqual([2, 1, 'E'])
     expect(move([2, 2, 'E'], 'f')).toEqual([3, 2, 'E'])
   })
 
-  it('When facing south and moving fordward, decrease the y coord (1)', () => {
+  it('When facing south and moving fordward, decrease the y coord', () => {
     expect(move([1, 1, 'S'], 'f')).toEqual([1, 0, 'S'])
     expect(move([2, 2, 'S'], 'f')).toEqual([2, 1, 'S'])
   })
 
-  it('When facing west and moving fordward, decrease the x coord (1)', () => {
+  it('When facing west and moving fordward, decrease the x coord', () => {
     expect(move([1, 1, 'W'], 'f')).toEqual([0, 1, 'W'])
     expect(move([2, 2, 'W'], 'f')).toEqual([1, 2, 'W'])
   })
