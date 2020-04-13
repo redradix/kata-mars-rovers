7cd4f072b214fc6cffcd682c7c120c9a7c6bba92
Sonia Ruiz <yune.vk@gmail.com>
2020-04-13 14:32:55 +0200

    [RED] Turn left when direction is south

--- src/index.test.js
+++ src/index.test.js
@@ -86,6 +86,11 @@ describe('Mars Rovers', () => {
       title: "Turn LEFT when direction is NORTH should set direction to WEST",
       input: [[0, 0, 'N'], 'l'],
       output: [0, 0, 'W']
+    },
+    {
+      title: "Turn LEFT when direction is SOUTH should set direction to EAST",
+      input: [[0, 0, 'S'], 'l'],
+      output: [0, 0, 'E']
     }
   ]
 
