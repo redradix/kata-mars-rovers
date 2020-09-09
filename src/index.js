3b5e931ec2aa5d8fd87ab639e0a34d2dabbd578c
Roberto Redradix <69057305+roberto-red@users.noreply.github.com>
2020-09-09 15:04:48 +0200

    [refactor] Rover throws an exception for invalid initial position

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-function createRoverCommander (gridSize, initialPosition, initialFacing) {
-  const [ height, width ] = gridSize
-  const [ row, column ] = initialPosition
+const isInGrid = ([ height, width ], [ row, column ]) => {
+  if (row < 0 || row >= height) return false
+  if (column < 0|| column >= width) return false
+  return true
+}
 
-  if (row < 0 || column < 0 || row >= height || column >= width) {
+function createRoverCommander (gridSize, initialPosition, initialFacing) {
+  if (!isInGrid(gridSize, initialPosition))
     throw new Error('Initial position is not inside grid boundaries')
-  }
 
   return function roverCommander(commands) {
     return {
