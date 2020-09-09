d265becacddc7024cc8a6bfcb4d6b5c6d028deb1
Roberto Redradix <69057305+roberto-red@users.noreply.github.com>
2020-09-09 14:38:43 +0200

    Rename expected results in tests to be more legible

--- src/index.test.js
+++ src/index.test.js
@@ -2,31 +2,31 @@ const { createRoverCommander } = require('./index')
 
 describe('Mars Rovers', () => {
   test('faces the initial facing when no command is given', () => {
-    const expected = [ 'N', 'E', 'S', 'W' ]
+    const expectedFacing = [ 'N', 'E', 'S', 'W' ]
 
-    const roverCommanders = expected
+    const roverCommanders = expectedFacing
       .map((direction) => createRoverCommander([0, 0], direction))
 
     roverCommanders.forEach((roverCommander, i) => {
       const { facing } = roverCommander('')
 
-      expect(facing).toBe(expected[i])
+      expect(facing).toBe(expectedFacing[i])
     })
   })
 
   test('stays in the initial position when no command is given', () => {
-    const expected = [
+    const expectedPosition = [
       [0, 0],
       [5, 5],
     ]
 
-    const roverCommanders = expected
+    const roverCommanders = expectedPosition
       .map((position) => createRoverCommander(position, 'S'))
 
     roverCommanders.forEach((roverCommander, i) => {
       const { position } = roverCommander('')
 
-      expect(position).toBe(expected[i])
+      expect(position).toBe(expectedPosition[i])
     })
   })
 })
