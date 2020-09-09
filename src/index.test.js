4c685933e3d21cb2d2aefecd97b84d5f2430d87d
Roberto Redradix <69057305+roberto-red@users.noreply.github.com>
2020-09-09 14:17:39 +0200

    [red] Rover stays in the initial position when no command is given

--- src/index.test.js
+++ src/index.test.js
@@ -5,12 +5,28 @@ describe('Mars Rovers', () => {
     const expected = [ 'N', 'E', 'S', 'W' ]
 
     const roverCommanders = expected
-      .map((direction) => createRoverCommander(direction))
+      .map((direction) => createRoverCommander([0, 0], direction))
 
     roverCommanders.forEach((roverCommander, i) => {
       const { facing } = roverCommander('')
 
       expect(facing).toBe(expected[i])
+    })
+  })
+
+  test('stays in the initial position when no command is given', () => {
+    const expected = [
+      [0, 0],
+      [5, 5],
+    ]
+
+    const roverCommanders = expected
+      .map((position) => createRoverCommander(position, 'S'))
+
+    roverCommanders.forEach((roverCommander, i) => {
+      const { position } = roverCommander('')
+
+      expect(position).toBe(expected[i])
     })
   })
 })
