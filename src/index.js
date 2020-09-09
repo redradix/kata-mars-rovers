f527c029e861dad77262bf71a645b304ef272424
Jaime Jacobo <jaimejacobordv@gmail.com>
2020-09-09 12:17:56 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,82 @@
+const createGrid = (sizes) => {
+  const grid = new Array()
+  for (let i = 0; i < sizes[0]; i++) {
+    const row = new Array()
+    for (let k = 0; k < sizes[1]; k++) {
+      row.push(0)
+    }
+    grid.push(row)
+  }
+  return grid
+}
+
+const setLocation = (startingPoint, initialDirection, grid) => grid[startingPoint[0]][startingPoint[1]] = initialDirection
+
+const changeDirection = (direction, command) => {
+  const directions = ['N', 'E', 'S', 'W']
+  const indexOfDirection = directions.indexOf(direction)
+  if (command === 'l') {
+    return indexOfDirection !== 0 ? directions[indexOfDirection - 1] : directions[3]
+  } else if (command === 'r') {
+    return indexOfDirection !== 3 ? directions[indexOfDirection + 1] : directions[0]
+  }
+}
+
+const checkForValidLocation = (previousLocation, newLocation) => {
+  return newLocation[0] < 0 || newLocation[1] < 0 ? previousLocation : newLocation
+}
+
+const moveRover = (previousLocation, direction, command) => {
+
+  let newLocation
+
+  let referenceLocations = [
+    [previousLocation[0] - 1, previousLocation[1]],
+    [previousLocation[0], previousLocation[1] + 1],
+    [previousLocation[0] + 1, previousLocation[1]],
+    [previousLocation[0], previousLocation[1] - 1]
+  ]
+
+  if (command === 'f') {
+    newLocation = {
+      N: referenceLocations[0],
+      E: referenceLocations[1],
+      S: referenceLocations[2],
+      W: referenceLocations[3]
+    }[direction]
+  } else if (command === 'b') {
+    newLocation = {
+      N: referenceLocations[2],
+      E: referenceLocations[3],
+      S: referenceLocations[0],
+      W: referenceLocations[1]
+    }[direction]
+  }
+  return checkForValidLocation(previousLocation, newLocation)
+}
+
+const getFinalGrid = (startingPoint, initialDirection, gridSize, commands) => {
+  let direction = initialDirection
+  let location = startingPoint
+
+  commands.forEach((command) => {
+    if (command === 'l' || command === 'r') {
+      direction = changeDirection(direction, command)
+    } else if (command === 'f' || command === 'b') {
+      location = moveRover(location, direction, command)
+    }
+  })
+
+  const finalGrid = createGrid(gridSize)
+  setLocation(location, direction, finalGrid)
+  return finalGrid
+}
+
+const rover = (startingPoint, initialDirection, gridSize, commands) => {
+  const grid = createGrid(gridSize)
+  setLocation(startingPoint, initialDirection, grid)
+
+  return getFinalGrid(startingPoint, initialDirection, gridSize, commands)
+}
+
+module.exports = rover
--- src/index.test.js
+++ src/index.test.js
@@ -1,3 +1,25 @@
+const rover = require('./index')
+
 describe('Mars Rovers', () => {
 
+
+  it('When moving, the rover stays in the same position if it encounters the grid\'s limit. ', () => {
+    expect(rover([1, 0], 'W', [2, 2], ['f'])[1][0]).toBe("W")
+  })
+
+  it('The direction the rover is facing changes correctly on simple instructions.', () => {
+    expect(rover([0, 0], 'E', [1, 1], ['r', 'r'])[0][0]).toBe("W")
+  })
+
+  it('The direction the rover is facing changes correctly when it turns more than 360 degrees. ', () => {
+    expect(rover([0, 0], 'S', [1, 1], ['l', 'l', 'l', 'l', 'l', 'l'])[0][0]).toBe("N")
+  })
+
+  it('A full set of instructions is completed correctly ', () => {
+
+    const grid = rover([2, 5], 'N', [4, 7], ['l', 'f', 'f', 'l', 'b', 'b', 'b', 'b', 'l', 'f', 'f'])
+    const validRow = grid.filter((row) => row.includes('E'))
+
+    expect(validRow.length === 1 && validRow[0][5] === 'E').toBe(true)
+  })
 })
