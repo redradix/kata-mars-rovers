a60a9731159d841555849ff7ba6e62d0ab5b73f1
Roberto Redradix <69057305+roberto-red@users.noreply.github.com>
2020-09-09 13:35:22 +0200

    [red] Rover faces initial facing if no command is given

--- src/index.test.js
+++ src/index.test.js
@@ -1,3 +1,12 @@
-describe('Mars Rovers', () => {
+const { createRoverCommander } = require('./index')
 
+describe('Mars Rovers', () => {
+  test('faces the initial facing when no command is given', () => {
+    const expected = 'N'
+    const roverCommander = createRoverCommander('N')
+
+    const result = roverCommander('').facing
+
+    expect(result).toBe(expected)
+  })
 })
