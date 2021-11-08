2e94e7dec589b04db2949f726275f399aca550f9
Virgilio Gómez <virgilio@redradix.com>
2021-11-08 16:36:43 +0100

    [GREEN] turn right

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 class Rover {
-  constructor() {
+  constructor(params) {
+    this.position = params.startingPoint
+    this.direction = params.direction
+    this.gridSize = params.gridSize
+  }
+
+  runCommands(commands) {
+    if (commands[0] === 'r') {
+      this.direction = 'E'
+    }
   }
 }
 
