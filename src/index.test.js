bd2c1702efb289ff85e874c87077b6c83b2e090c
Virgilio Gómez <virgilio@redradix.com>
2021-11-09 08:54:40 +0100

    [REFACTOR] rename tests description to fix syntax

--- src/index.test.js
+++ src/index.test.js
@@ -7,34 +7,33 @@ describe('Mars Rovers', () => {
     gridSize: [10, 10],
   }
 
-  it('turn right', () => {
+  it('turns right', () => {
     const rover = new Rover(initialParams)
     rover.runCommands(['r'])
     expect(rover.direction).toEqual('E')
   })
-  it('turn three times right and once left', () => {
+  it('turns right and left several times', () => {
     const rover = new Rover(initialParams)
     rover.runCommands(['r', 'r', 'r', 'l'])
     expect(rover.direction).toEqual('S')
   })
 
-  it('move forward looking to the east', () => {
+  it('moves forward looking to the east', () => {
     const rover = new Rover(initialParams)
     rover.runCommands(['r', 'f'])
     expect(rover.position).toEqual([1, 0])
   })
-  it('move backward looking to the west', () => {
+  it('moves backward looking to the west', () => {
     const rover = new Rover(initialParams)
     rover.runCommands(['l', 'b'])
     expect(rover.position).toEqual([1, 0])
   })
-
-  it('move forward looking to the west', () => {
+  it('moves forward looking to the west', () => {
     const rover = new Rover(initialParams)
     rover.runCommands(['r', 'f', 'r', 'r', 'f'])
     expect(rover.position).toEqual([0, 0])
   })
-  it('move backward looking to the east', () => {
+  it('moves backward looking to the east', () => {
     const rover = new Rover(initialParams)
     rover.runCommands(['r', 'f', 'b'])
     expect(rover.position).toEqual([0, 0])
