977b9e7b277e4072697f3215b51fd56ce6feeed4
Virgilio Gómez <virgilio@redradix.com>
2021-11-09 08:45:10 +0100

    [GREEN] move backward looking to the east

--- src/index.js
+++ src/index.js
@@ -33,6 +33,8 @@ class Rover {
         this.position[0]--
       } else if (command === 'b' && this.direction === 'W') {
         this.position[0]++
+      } else if (command === 'b' && this.direction === 'E') {
+        this.position[0]--
       }
     })
   }
